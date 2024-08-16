a1e1a1e2b0e978c094f8154dc13473e29118dbc8
Kimiyuki Yamauchi <kimiyuki.yamauchi.0313@gmail.com>
2024-08-16 17:08:10 +0900

    完成

--- app/src/main/java/com/example/listviewsample/MainActivity.java
+++ app/src/main/java/com/example/listviewsample/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.listviewsample;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +19,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.lvMenu), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // ListViewオブジェクトを取得
+        ListView lvMenu = findViewById(R.id.lvMenu);
+        // ListViewにリスナを設定
+        lvMenu.setOnItemClickListener(new ListItemClickListener());
+    }
+
+    // リストがタップされあたときの処理が記述されたメンバクラス
+    private class ListItemClickListener implements AdapterView.OnItemClickListener {
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            // タップされた定食名を取得
+            String item = (String) parent.getItemAtPosition(position);
+            // トーストで表示する文字列を生成
+            String show = "あなたが選んで定食：" + item;
+            // トーストの表示
+            Toast.makeText(MainActivity.this, show, Toast.LENGTH_LONG).show();
+        }
     }
 }
